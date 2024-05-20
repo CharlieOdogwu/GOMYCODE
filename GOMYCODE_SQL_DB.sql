e71e4827e2b77270fad6152515eda249c0b1b917
CharlieOdogwu <charlesiheoma100@gmail.com>
2024-05-20 09:47:38 +0100

    Add files via upload

--- GOMYCODE_SQL_DB.sql
+++ GOMYCODE_SQL_DB.sql
@@ -22,3 +22,39 @@ CREATE TABLE Orders(
   product_id INT FOREIGN KEY (product_id) REFERENCES Product (product_id) NOT NULL,
   quantity INT NOT NULL,
   order_date DATE NOT NULL);
+
+
+--CHECKPOINT FOR DML
+--INSERT INTO PRODUCT TABLE
+INSERT INTO Product (product_id, name, price)
+VALUES
+(1, 'Cookies', 10),
+(2, 'Candy', 5.2);
+
+--INSERT INTO CUSTOMER TABLE
+INSERT INTO Customer (customer_id, name, address)
+VALUES
+(1, 'Ahmed', 'Tunisia'),
+(2, 'Coulibaly', 'Senegal'),
+(3, 'Hasan', 'Eygpt');
+
+--INSERT INTO ORDER TABLE
+INSERT INTO Orders (order_id, customer_id, product_id, quantity, order_date)
+VALUES
+(1, 1, 2, 3, '2023-01-22'),
+(2, 2, 1, 10, '2023-04-14');
+
+--UPDATE THE ORDER TABLE
+UPDATE Orders
+SET quantity = 6
+WHERE order_id = 2 and customer_id = 2
+
+--DELETE FROM CUSTOMER TABLE
+DELETE FROM Customer
+WHERE customer_id = 3
+
+--DELETE FROM Orders
+DELETE FROM Orders;
+
+-- DROP THE ORDER TABLE
+DROP TABLE Orders;
